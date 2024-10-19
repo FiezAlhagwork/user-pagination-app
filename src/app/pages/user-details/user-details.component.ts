dc7309dc31e538a69ccde357c8edc2ee7ac6fd4b
FiezAlhagwork <148772477+FiezAlhagwork@users.noreply.github.com>
2024-10-19 13:50:18 +0300

    change files

--- src/app/pages/user-details/user-details.component.ts
+++ src/app/pages/user-details/user-details.component.ts
@@ -1,6 +1,6 @@
 //impoer angular and rxjs
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, Params} from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
@@ -13,62 +13,37 @@ import { UserState } from '../../../store/state/users.state';
 // import Material
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 
-import { CardUserComponent } from "../../components/card-user/card-user.component";
-
+import { CardUserComponent } from '../../components/card-user/card-user.component';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [CardUserComponent,CommonModule,MatProgressBarModule],
+  imports: [CardUserComponent, CommonModule, MatProgressBarModule],
   templateUrl: './user-details.component.html',
-  styleUrl: './user-details.component.css'
+  styleUrl: './user-details.component.css',
 })
-export class UserDetailsComponent  implements OnInit {
-  private activadRoute = inject(ActivatedRoute) ;
-  private store = inject(Store)
+export class UserDetailsComponent implements OnInit {
+  private activadRoute = inject(ActivatedRoute);
+  private store = inject(Store);
 
-  data!:UsersDetails
+  data!: UsersDetails;
 
-  user$ :Observable<UsersDetails> = this.store.select(UserState.getUser)
-  loading:boolean = true
-  error:string = ""
-
+  user$: Observable<UsersDetails> = this.store.select(UserState.getUser);
 
   ngOnInit(): void {
-    this.loading =  true
-    this.activadRoute.params.subscribe((res:Params) => {
-      const userId = Number(res['id'])
-      if(userId){
-        this.GetUser(userId)
+    this.activadRoute.params.subscribe((res: Params) => {
+      const userId = Number(res['id']);
+      if (userId) {
+        this.GetUser(userId);
       }
-      else{
-        this.error = 'Invalid ID'
-        this.loading = false
-      }
-      
-    })
+    });
 
-
-
-
-
-    
-    
     this.user$.subscribe((data) => {
-          if(data){
-            this.data = data
-            this.loading = false
-          }else{
-            this.loading = false
-          }
-    })
+      this.data = data;
+    });
   }
 
-  GetUser(id:number){
-    this.store.dispatch(new GetUserDetails(id))
+  GetUser(id: number) {
+    this.store.dispatch(new GetUserDetails(id));
   }
-
-  
-
-
 }
